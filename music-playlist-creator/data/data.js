041d30b7c2111c4e7a01beba0e4f34ad8b5f8e6c
Sachi Jitendra Hansalia <sachih@meta.com>
2025-06-04 22:38:33 -0700

    data can read from json but onclick doesn't work yet

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,36 @@
+const playlistData = [{
+    playlistID: 1,
+    playlist_name: "My Playlist #1",
+    playlist_author: "Sachi",
+    playlist_art: "assets\\img\\playlist.png",
+    playlistLikes: 1,
+    songs: [
+        {songTitle: "Song Title", artistName: "Artist Name", albumName: "Album Name", runTime: "0:00"},
+        {songTitle: "Song Title", artistName: "Artist Name", albumName: "Album Name", runTime: "0:00"},
+        {songTitle: "Song Title", artistName: "Artist Name", albumName: "Album Name", runTime: "0:00"}
+    ]
+},
+{
+    playlistID: 2,
+    playlist_name: "Summer Playlist",
+    playlist_author: "Joe",
+    playlist_art: "assets\\img\\playlist.png",
+    laylistLikes: 12,
+    songs: [
+        {songTitle: "Song Title", artistName: "Artist Name", albumName: "Album Name", runTime: "0:00"},
+        {songTitle: "Song Title", artistName: "Artist Name", albumName: "Album Name", runTime: "0:00"},
+        {songTitle: "Song Title", artistName: "Artist Name", albumName: "Album Name", runTime: "0:00"}
+    ]
+},
+{
+    playlistID: 1,
+    playlist_name: "Carpool Karaoke",
+    playlist_author: "Jimmy",
+    playlist_art: "assets\\img\\playlist.png",
+    playlistLikes: 3,
+    songs: [
+        {songTitle: "Song Title", artistName: "Artist Name", albumName: "Album Name", runTime: "0:00"},
+        {songTitle: "Song Title", artistName: "Artist Name", albumName: "Album Name", runTime: "0:00"},
+        {songTitle: "Song Title", artistName: "Artist Name", albumName: "Album Name", runTime: "0:00"}
+    ]
+}]
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+   loadPlaylistPage()
+})
 
+function createPlaylistCard(card){
+   let newCard = document.createElement("div")
+   newCard.className = "card"
+   newCard.onclick = "openModal()"
+   newCard.innerHTML = `
+      <img class="playlist-image" src="${card.playlist_art}">
+      <h4>${card.playlist_name}</h4>
+      <p>${card.playlist_author}</p>
+      <div class="likes">
+         <button class="likebutton">Likes: ${card.playlistLikes}</button>
+      </div>
+   `
+   return newCard
+}
+
+function loadPlaylistPage(){
+   const newplaylistData = document.getElementById("playlist-cards")
+   playlistData.forEach( playlist => {
+      const elem = createPlaylistCard(playlist)
+      newplaylistData.appendChild(elem)
+   })
+}
+
+
+
+
+//Modal Script
+const modal = document.getElementById("playlistModal");
+const span = document.getElementsByClassName("close")[0];
+
+function openModal(/*playlist*/) {
+   //document.getElementById('playlistName').innerText = playlist.name;
+   //document.getElementById('playlistImage').src = playlist.imageUrl;
+   //document.getElementById('creatorName').innerText = playlist.creatorName;
+   //document.getElementById('song-car').innerHTML = `${playlist.songs.join(', ')}`;
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
